3bfd81918c0ad7f984565dad9a44543111b428e3
avolent <will@slamped.com>
2022-02-20 19:58:23 +1100

    inbox and compose complete

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,6 +5,31 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  
+  // On Compose submit
+  document.querySelector('#compose-form').onsubmit = () => {
+    console.log('Send email');
+    const recipients = document.querySelector('#compose-recipients').value;
+    const subject = document.querySelector('#compose-subject').value;
+    const body = document.querySelector('#compose-body').value;
+    console.log(recipients);
+    console.log(subject)
+    console.log(body)
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: recipients,
+          subject: subject,
+          body: body
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        console.log(result);
+    });
+  }
+  
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -20,6 +45,7 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +56,19 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  
+  // Load the emails
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    // Print emails
+    console.log(emails);
+    // ... do something else with emails ...
+    emails.forEach(element => {
+      const email = document.createElement("div");
+      email.className  = "email"
+      email.innerHTML = `<div class="sender">${element.sender}</div><div class="subject">${element.subject}</div><div class="timestamp">${element.timestamp}</div>`;
+      document.querySelector('#emails-view').append(email);
+    });
+  });
 }
